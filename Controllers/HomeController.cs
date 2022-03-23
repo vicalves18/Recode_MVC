54086a7f092b07e82b57992e7eb2471f5540dca9
Victória Alves <vic.alves200216@gmail.com>
2022-03-23 19:35:19 -0300

    adicionando contatos

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,6 +27,7 @@ namespace Recode_MVC.Controllers
         {
             return View();
         }
+        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
